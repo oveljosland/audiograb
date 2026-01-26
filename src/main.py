983f012e20d59d8b72b96219119f2d4fb54df6ae
oveljosland <git@ljosland.co>
2026-01-27 00:26:39 +0100

    add test for google cloud storage and some utilities

--- src/main.py
+++ src/main.py
@@ -1,33 +1,44 @@
 #!/usr/bin/env python3
 #python -m src.main
-
+import os
 import time
 import subprocess
-from .utils import getmnt
+#from .utils import getmnt
+from google.cloud import storage
 
-def sysconfig(sd_mount_directory, sd_card):
-	working_directory_name = 'monitoring-tmp'
-	upload_directory_name = 'audio'
-	working_directory = os.path.join('/tmp', working_directory_name)
-	local_upload_directory = upload_directory_name
+#CONFIG_FILE_NAME = 'config.json'
 
-	if sd_card:
-		upload_directory = os.path.join(sd_mount_directory, upload_directory_name)
-		
-		if os.path.exists(local_upload_directory) and os.path.isdir(local_upload_directory):
-			# TODO: merge dirs
-	else:
-		upload_directory = local_upload_directory
 
-	project_id = 'NA'
-	config_id = 'NA'
-	cpu_serial = get_cpu_serial()
+
+
+def upload_directory_to_bucket(upload_directory, bucket):
+	uploaded = []
+	for root, _, files in os.walk(upload_directory):
+		for local_file in files:
+			local_path = os.path.join(root, local_file)
+			remote_path = local_path[len(upload_directory)+1:]
+			blob = bucket.blob(remote_path)
+			blob.upload_from_filename(local_path)
+			uploaded.append(local_path)
+	return uploaded
+
 
 if __name__ == "__main__":
 	start = time.strftime("%Y-%m-%d %H:%M:%S")
+
+	"""
 	mnt = getmnt()
 	if mnt:
 		print(f"mnt: {mnt}")
 	else:
 		# TODO: log: not mounted
 		print("not mounted")
+	"""
+
+	test_dir = "src/test/upload"
+
+	client = storage.Client.from_service_account_json("src/gcs_sa.json")
+	bucket = client.bucket("audiograb-development")
+	uploaded = upload_directory_to_bucket(test_dir, bucket)
+	print(f"uploaded {len(uploaded)} files")
+
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,40 @@
 # util
+import socket
+
+INET_RETRIES = 15
+
+
+
+def sysconfig(sd_mount_directory, sd_card):
+	working_directory_name = 'monitoring-tmp'
+	upload_directory_name = 'audio'
+	working_directory = os.path.join('/tmp', working_directory_name)
+	local_upload_directory = upload_directory_name
+
+	if sd_card:
+		upload_directory = os.path.join(sd_mount_directory, upload_directory_name)
+		
+		if os.path.exists(local_upload_directory) and os.path.isdir(local_upload_directory):
+			# TODO: merge dirs (Why)
+			pass
+	else:
+		upload_directory = local_upload_directory
+
+	project_id = 'NA'
+	config_id = 'NA'
+	cpu_serial = get_cpu_serial()
+
+	if os.path.exists(CONFIG_FILE_NAME):
+		device_config = json.load(open(CONFIG_FILE_NAME))['device']
+		project_id = device_config['project_id']
+		config_id = device_config['config_id']
+
+	project_directory = os.path.join(upload_directory, 'project_{}'.format(project_id))
+	device_directory = os.path.join(project_directory, 'device_{}'.format(cpu_serial))
+	data_directory = os.path.join(device_directory, 'config_{}'.format(config_id))
+
+	return working_directory, upload_directory, data_directory
+
 
 # get mount point of removable disk
 def getmnt():
@@ -27,3 +63,42 @@ def get_cpu_serial():
 	except OSError:
 		pass
 	return error
+
+
+def has_internet_access(timeout=3):
+	try:
+		socket.setdefaulttimeout(timeout)
+		socket.create_connection(("1.1.1.1", 53))
+		return True
+	except OSError:
+		return False
+
+def wait_for_internet(tries, timeout=2):
+	connected = False
+	for t in range(tries):
+		connected = has_internet_access(timeout)
+	
+		if connected:
+			break
+	
+		else:
+			# should maybe log some of thisv
+			time.sleep(1)
+	
+	if connected:
+		print("connected to the internet")
+		# light up some LEDs
+	else:
+		print(f"no connection after {t}/{tries}")
+
+	return connected
+
+
+	
+def time_synced():
+	output = subprocess.run(
+		["timedatectl", "show", "-p", "NTPSynchronized", "--value"],
+		capture_output=True,
+		text=True
+	)
+	return output.stdout.strip() == "yes"
