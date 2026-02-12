a58c9ce0fb606a3e8654214e21adcf7226a2e1b6
oveljosland <git@ljosland.co>
2026-02-12 23:06:22 +0100

    remove setup_directories. replaced by create_upload_directory

--- src/utils/other.py
+++ src/utils/other.py
@@ -8,37 +8,6 @@ import subprocess
 CONFIG_FILE_NAME = 'src/config/example.json'
 
 
-def setup_directories(sd_mount_directory, sd_card):
-	working_directory_name = 'monitoring-tmp'
-	upload_directory_name = 'audio'
-	working_directory = os.path.join('/tmp', working_directory_name)
-	local_upload_directory = upload_directory_name
-
-	if sd_card:
-		upload_directory = os.path.join(sd_mount_directory, upload_directory_name)
-		
-		if os.path.exists(local_upload_directory) and os.path.isdir(local_upload_directory):
-			# TODO: merge dirs (Why)
-			pass
-	else:
-		upload_directory = local_upload_directory
-
-	project_id = 'NA'
-	config_id = 'NA'
-	cpu_serial = read_cpu_serial()
-
-	if os.path.exists(CONFIG_FILE_NAME):
-		with open(CONFIG_FILE_NAME) as f:
-			device_config = json.load(f)['device']
-			project_id = device_config['project_id']
-			config_id = device_config['config_id']
-
-	project_directory = os.path.join(upload_directory, 'project_{}'.format(project_id))
-	device_directory = os.path.join(project_directory, 'device_{}'.format(cpu_serial))
-	data_directory = os.path.join(device_directory, 'config_{}'.format(config_id))
-
-	return working_directory, upload_directory, data_directory
-
 
 def read_cpu_serial():
 	error = "NO_CPU_SERIAL"
