6d50cecab15e0df8533598390339154a1d2aebbc
oveljosland <git@ljosland.co>
2026-02-23 17:30:36 +0100

    add doc

--- src/utils/bat.py
+++ src/utils/bat.py
@@ -1,6 +1,9 @@
 import subprocess
 
 def get_voltage():
+	"""
+	https://www.raspberrypi.com/documentation/computers/os.html#vcgencmd
+	"""
 	output = subprocess.run([
 		"vcgencmd", "pmic_read_adc", "BATT_V"
 		],
