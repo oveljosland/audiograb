06bb77effbb52cf5c57ef3e711c735cf22212313
ove <git@ljosland.co>
2026-03-10 18:46:42 +0100

    Revert "moved python source code for the daemon into  and moved C source code into"
    This reverts commit 6b94e1aa0d48bd78a184cd0ca10a56b9abf59777.
    
    accidentally removed the files again

---
+++ src/utils/bat.py
@@ -0,0 +1,16 @@
+import subprocess
+
+def get_battery_voltage():
+	"""
+	https://www.raspberrypi.com/documentation/computers/os.html#vcgencmd
+	"""
+	output = subprocess.run([
+		"vcgencmd", "pmic_read_adc", "BATT_V"
+		],
+		capture_output=True,
+		check=True,
+		text=True
+	)
+	output = output.stdout.strip()
+	output = output.split("=")[1].removesuffix('V')
+	return output
